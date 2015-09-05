481e0096ac5bafe1bd734c93702f38c03a118055
blaise <blaisehorvath@gmail.com>
2015-09-05 16:51:03 +0200

    Added source files

---
+++ src/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express')
+const handlebars  = require('express-handlebars');
+const path = require('path');
+const appDirName = path.dirname(require.main.filename);
+
+
+const app = express();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', appDirName+"/views/");
+
+app.get('/', function (req, res) {
+    res.render("home", {layout:false, title:"Welcome to the algorithms course in Javascript"});
+});
+
+app.listen(4444, function () {
+    console.log('Server started!');
+    console.log('Listening on port: 4444');
+});
