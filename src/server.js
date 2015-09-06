cb9bc7ece8ac22a38d1aa38340971bbd277b42f9
blaise <blaisehorvath@gmail.com>
2015-09-06 17:46:32 +0200

    Configured the public (static) libary, where the css, and js files will be stored

--- src/server.js
+++ src/server.js
@@ -3,23 +3,29 @@
 const express = require('express')
 const handlebars  = require('express-handlebars');
 const path = require('path');
-const appDirName = path.dirname(require.main.filename);
 
+//the absolute location of this file
+const appDirName = path.dirname(require.main.filename);
 
 const app = express();
 
+//configuring handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+//setting the view engine of the app
 app.set('view engine', 'handlebars');
+//setting up the folder where the app looks up views
 app.set('views', appDirName+"/views/");
+//setting the static directory
+app.use(express.static(appDirName+"/static"));
 
 app.get('/', function (req, res) {
-    res.render("home", {layout:false,
-                        title: "Algorithms in Javascript",
-                        headerTitle: "Welcome to the algorithms course in Javascript"
+    res.render("home", {
+                              layout:false,
+                              title: "Algorithms in Javascript",
+                              headerTitle: "Welcome to the algorithms course in Javascript"
                         });
 });
 
 app.listen(4444, function () {
-    console.log('Server started!');
-    console.log('Listening on port: 4444');
+    console.log('Server started! \r\nListening on port: 4444');
 });
