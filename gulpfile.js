dd15d27616e63b4152102e3a14da6d65aed0fc8d
blaise <blaisehorvath@gmail.com>
2015-09-10 19:51:15 +0200

    Added a new task that copies the site's own css to the build/css directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,24 @@ var gutil = require('gulp-util');
 var babel = require('gulp-babel');
 var server = require('gulp-develop-server');
 
+//building the server
 gulp.task('build_server', function(){
-   return gulp.src('src/server.js').pipe(babel()).pipe(gulp.dest('build'))
+   return gulp.src('src/server.js').pipe(babel()).pipe(gulp.dest('build'));
 });
 
+//copying css files
+gulp.task('copy_css', function(){
+      return gulp.src('src/css/own.css').pipe(gulp.dest('build/static/css/'));
+});
+
+//copying views
 gulp.task('copy_views', function(){
-   return gulp.src('src/views/*.jsx').pipe(gulp.dest('build/views'))
+   return gulp.src('src/views/*.jsx').pipe(gulp.dest('build/views'));
 });
 
+//copying layouts
 gulp.task('copy_layouts', function(){
-      return gulp.src('src/views/layouts/*.jsx').pipe(gulp.dest('build/views/layouts'))
+      return gulp.src('src/views/layouts/*.jsx').pipe(gulp.dest('build/views/layouts'));
 });
 
 // run server
@@ -25,6 +33,7 @@ gulp.task( 'server:start', function() {
     //if any of the views changed, the old views are overwritten and the server is restarted
     gulp.watch( ['src/views/*.jsx'], ['copy_views', server.restart]);
     gulp.watch( ['src/views/layouts/*.jsx'], ['copy_layouts', server.restart]);
+    gulp.watch( ['src/css/own.css'], ['copy_css', server.restart]);
 });
 
 // restart server if app.js changed
@@ -32,4 +41,4 @@ gulp.task( 'server:restart', function() {
     gulp.watch( [ 'src/server.js' ], server.restart );
 });
 
-gulp.task('default', ['build_server', 'copy_views', 'copy_layouts', 'server:start']);
+gulp.task('default', ['build_server', 'copy_views', 'copy_layouts', 'copy_css', 'server:start']);
