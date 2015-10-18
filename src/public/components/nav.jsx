cc1101ea1a959d60d19f59a26e74e93db37e83b2
blaise <blaisehorvath@gmail.com>
2015-10-18 14:48:51 +0200

    Changed the style ofthe footer

--- src/public/components/nav.jsx
+++ src/public/components/nav.jsx
@@ -5,7 +5,7 @@ let React = require("react");
 let Nav = React.createClass({
       render: () => {
             return (
-                  <nav className="navbar navbar-default">
+                  <nav className="navbar navbar-default navbar-fixed-top">
                     <div className="container-fluid">
                       <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
