56741aa5840141ae67804fc679f5094630a6ecca
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2016-08-01 15:34:12 +0200

    added https support, working on Admin ajax

--- src/public/components/Admin.jsx
+++ src/public/components/Admin.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-
+const func = ()=>{
+    console.log("ajaxing");
+    $.ajax({
+        type: "POST",
+        url: "/adminfaszpinafaszfaszhitlerPOLPOT",
+        data:"SOKFASZ",
+        //data: $("#idForm").serialize(), // serializes the form's elements.
+        success: function(data)
+        {
+            alert(data); // show response from the php script.
+        }
+    });
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+};
 
 //TODO:remove brs...
 const Admin = ()=>{
     return(
         <div>
-                <form method="post" action="/admin">
+                <form id="idForm" onsubmit={()=>{console.log("fasz")}}>
                     <input type="text" placeholder="User" name="user"/>
                     <br/>
                     <input type="password" placeholder="Password" name="password"/>
                     <br/>
-                    <input type="submit" value="Submit"/>
+                    <button type="button" onClick={func}>Submit</button>
+                    <br/>
+                    <input type="submit" value="Submit" onClick="console.log("fasz")"/>
                 </form>
         </div>
+
 )};
 export default Admin
