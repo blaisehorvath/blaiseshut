42a6e5d500cd91c568cb34c38c0ef1aa72ded27e
blaisehorvath <blaisehorvath@gmail.com>
2016-09-08 09:11:13 +0200

    Changed the look of the Blog, and corrected the relocation bug. Now the browser history is not cleared when the user goes from the blog to a blogpost page.

--- src/public/components/BlogPost.jsx
+++ src/public/components/BlogPost.jsx
@@ -2,23 +2,50 @@ import React from "react"
 import {loadBlogPost} from '../reducers/StoreAndReducers'
 import {connect} from 'react-redux'
 import page from 'page'
-const BlogPost = ({post, loggedIn,dispatch})=> {
+const BlogPost = ({post, loggedIn, dispatch})=> {
     return <div className="blogPost panel panel-default">
         <div className="panel-heading">
             <div className="row">
-                <div className="postAuthor col-xs-8">
-                    <div>Post by {post.user}</div>
-                    <h2>{post.title}</h2>
+                <div className="col-xs-12">
+                    <h2 className="postTitle" onClick={goToBlogpost.bind({}, post, dispatch)}>{post.title}</h2>
                 </div>
-                <div className="postDate col-xs-4 pull-right">Created on {post.date}</div>
+            </div>
+            <div className="row">
+                <div className="postAuthor col-xs-6">Post by <i>{post.user}</i></div>
+                <div className="postDate col-xs-6">{post.date}</div>
             </div>
         </div>
-        <div className="panel-body">{post.text}</div>
+        <div className="panel-body">
+            {post.text}
+
+            <a className="readMore" onClick={goToBlogpost.bind({}, post, dispatch)}>
+                Read more!
+            </a>
+
+            {loggedIn ? <a onClick={()=> {
+                dispatch(loadBlogPost(post));
+                page.redirect("/admin/" + encodeURIComponent(post.title))
+            }}> Edit!</a> : ""}
+
+        </div>
         <div className="panel-footer">
-            {post.tags.toString()+"          "}
-            <a onClick={()=>{dispatch(loadBlogPost(post));page.redirect("/blog/" + encodeURIComponent(post.title))}}>Read more!</a>
-            {loggedIn?<a onClick={()=>{dispatch(loadBlogPost(post));page.redirect("/admin/" + encodeURIComponent(post.title))}}>          Edit!</a>:""}
+            <span className="glyphicon glyphicon-tags"/>
+            {post.tags.toString()}
+
         </div>
     </div>
 };
+
+/**
+ * This function uses the page front-end router module to go to another blogpost.
+ * Warning! Don't use page.redirect function for this, because is cleans
+ * the browser history and the user can't come back
+ * @param post {Object} An object that contains the blogpost
+ * @param dispatch {function} A function that informs the store about the change
+ */
+const goToBlogpost = (post, dispatch) => {
+    dispatch(loadBlogPost(post));
+    page("/blog/" + encodeURIComponent(post.title));
+};
+
 export default connect()(BlogPost)
