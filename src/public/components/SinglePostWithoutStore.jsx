3e4a3545d9535c437959089eade048bb7fdd00b7
blaisehorvath <blaisehorvath@gmail.com>
2016-09-09 13:01:49 +0200

    Added divs and stuff to SinglepostWithoutStore.jsx

--- src/public/components/SinglePostWithoutStore.jsx
+++ src/public/components/SinglePostWithoutStore.jsx
@@ -1,12 +1,32 @@
 import React from "react"
 const SinglePostWithoutStore = ({blogPost})=> {
-    return <div><br/><br/><br/><br/><br/>
-        <div>{blogPost.title}</div>
-        <div>{blogPost.user}</div>
-        <div>{blogPost.date}</div>
-        <div>{blogPost.precontent}</div>
-        <div>{blogPost.text}</div>
-        <div>{blogPost.tags}</div>
-    </div>
+    //converting the blogpost's date to a human readable format
+    let date = new Date(blogPost).toDateString();
+    return (
+        <section id="singleBlogPost">
+            <div className="container">
+                <div className="postHeader row">
+                    <div className="col-12">
+                        <h1 className="postTitle">{blogPost.title}</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <div>{blogPost.user}</div>
+                    </div>
+                    <div className="col-6">
+                        {date}
+                    </div>
+                </div>
+                <div className="row postContent">
+                    <div className="col-12">{blogPost.precontent}</div>
+                    <div className="col-12">{blogPost.text}</div>
+                </div>
+                <div className="postFooter row">
+                    <span className="postTagsIcon glyphicon glyphicon-tags"></span><span>{blogPost.tags}</span>
+                </div>
+            </div>
+        </section>
+    )
 };
 export default SinglePostWithoutStore
