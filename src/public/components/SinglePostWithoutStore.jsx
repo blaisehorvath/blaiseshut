b1a61343aa184b4c45626499103081836801dc61
blaisehorvath <blaisehorvath@gmail.com>
2016-09-09 18:18:19 +0200

    Separated the css mixins and variables to separate files. Added a lot of styles and removed the stroke effect from all of the headers.

--- src/public/components/SinglePostWithoutStore.jsx
+++ src/public/components/SinglePostWithoutStore.jsx
@@ -1,26 +1,28 @@
 import React from "react"
 const SinglePostWithoutStore = ({blogPost})=> {
     //converting the blogpost's date to a human readable format
-    let date = new Date(blogPost).toDateString();
+    let date = new Date(blogPost.date).toDateString();
     return (
         <section id="singleBlogPost">
             <div className="container">
-                <div className="postHeader row">
-                    <div className="col-12">
-                        <h1 className="postTitle">{blogPost.title}</h1>
+                <div className="postHeader">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h1 className="postTitle">{blogPost.title}</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6 postAuthor">
+                            <div>Post by {blogPost.user}</div>
+                        </div>
+                        <div className="col-sm-6 pull-right postDate">
+                            <div>{date}</div>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-6">
-                        <div>{blogPost.user}</div>
-                    </div>
-                    <div className="col-6">
-                        {date}
-                    </div>
-                </div>
-                <div className="row postContent">
-                    <div className="col-12">{blogPost.precontent}</div>
-                    <div className="col-12">{blogPost.text}</div>
+                <div className="row postBody">
+                    <div className="col-sm-12">{blogPost.precontent}</div>
+                    <div className="col-sm-12">{blogPost.text}</div>
                 </div>
                 <div className="postFooter row">
                     <span className="postTagsIcon glyphicon glyphicon-tags"></span><span>{blogPost.tags}</span>
