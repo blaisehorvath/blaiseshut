64cd4992b7217ae8f19a2478e005bbe1cdacd59b
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2016-09-10 16:12:14 +0200

    Caching is ready with blogPosts.

--- src/server/db.js
+++ src/server/db.js
@@ -5,11 +5,23 @@ import AWS from "aws-sdk";
 import React from "react";
 /*App*/
 import hipsteripsom from 'hipsteripsum'
-import BlogPost from "../public/pages/BlogPost"
+
 
 import {store} from './server'
-import {setInitialTags} from "../public/reducers/StoreAndReducers"
+import {Provider} from 'react-redux'
+import renderHTML from "./renderHTML"
 
+import ReactApp from "../public/components/ReactApp";
+import About from "../public/pages/About"
+import Admin from "../public/pages/Admin"
+import AdminLoggedIn from "../public/pages/AdminLoggedIn"
+import Blog from "../public/pages/Blog"
+import BlogPost from "../public/pages/BlogPost"
+import AppReducer from "../public/reducers/StoreAndReducers"
+
+import _ from "ramda"
+import {createStore} from 'redux';
+import ReactDOM from "react-dom/server"
 ///*Setting up amazon AWS connection.
 // Should have a credentials file in ~/.aws with the following content:
 /*[default]
@@ -35,25 +47,28 @@ let params = {
     Count: true
 };
 let queryCache = undefined;
-(()=> {
-    let queryparams = {
-        TableName: "SWAblog",
-        ProjectionExpression: "#id, #date, #text, #user, #tags, #precontent, #title",
-        ExpressionAttributeNames: {
-            "#id": "id",
-            "#date": "date",
-            "#text": "text",
-            "#user": "user",
-            "#tags": "tags",
-            "#precontent": "precontent",
-            "#title": "title"
-        }
-    };
-    doc.scan(queryparams, (err, data)=> {
-        if (err) console.log(err);
-        else queryCache = data;
-    });
-})();
+let fillQueryCache = ()=> {
+    return new Promise((res, rej)=> {
+        let queryparams = {
+            TableName: "SWAblog",
+            ProjectionExpression: "#id, #date, #text, #user, #tags, #precontent, #title",
+            ExpressionAttributeNames: {
+                "#id": "id",
+                "#date": "date",
+                "#text": "text",
+                "#user": "user",
+                "#tags": "tags",
+                "#precontent": "precontent",
+                "#title": "title"
+            }
+        };
+        doc.scan(queryparams, (err, data)=> {
+            if (err) console.log(err);
+            else queryCache = data;
+            res(data);//TODO:UGLY!
+        })
+    })
+};
 //TODO:GET TAGS BEFORE SENDIND THE STORE TO ANYONE!!
 const getBlogPostByTitle = (title)=>queryCache.Items.filter(post=>post.title === title)[0];
 const queryBlogPosts = (currentBlogPostIds, activeTags, numberOfPostsToReturn)=> {//
@@ -146,6 +161,229 @@ const getTags = ()=> {
             return tags
         })
 };
+const pathsAndStores = {
+    '/': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        },
+    },
+    '/admin': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true});
+            this.content =
+                ReactDOM.renderToString(
+                    <Provider store={store}><ReactApp><Admin/></ReactApp></Provider>);
+            this.contentLogged =
+                ReactDOM.renderToString(
+                    <Provider store={store}><ReactApp><AdminLoggedIn></AdminLoggedIn></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/admin/:blogTitle': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true});
+            this.content =
+                ReactDOM.renderToString(
+                    <Provider store={store}><ReactApp><Admin/></ReactApp></Provider>);
+            this.contentLogged =
+                ReactDOM.renderToString(<Provider
+                    store={store}><ReactApp><AdminLoggedIn></AdminLoggedIn></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/aboutUs': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false, activeMenuButton: "aboutUs"});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true, activeMenuButton: "aboutUs"});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/projects': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false, activeMenuButton: "projects"});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true, activeMenuButton: "projects"});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/team': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false, activeMenuButton: "team"});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true, activeMenuButton: "team"});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/contactUs': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags, LoggedIn: false, activeMenuButton: "contactUs"});
+            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true, activeMenuButton: "contactUs"});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        }
+    },
+    '/blog': {
+        init: function () {
+            this.store = createStore(AppReducer, {Tags,});
+            this.storeLogged = createStore(AppReducer, {Tags});
+            this.content = ReactDOM.renderToString(
+                <Provider store={this.store}><ReactApp><Blog/></ReactApp></Provider>);
+            this.contentLogged = ReactDOM.renderToString(
+                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
+            this.response = renderHTML(this.content, this.store.getState());
+            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
+            this.getResponse = function (loggedIn) {
+                return loggedIn ? this.responseLogged : this.response;
+            };
+            return this;
+        },
+    },
+    '/blog/:blogTitle': {
+        //TODO: encodeURIComponent isn't needed, the request contains the encoded blogTitle already.
+        init: function () {
+            this.stores = {
+                initStore: function () {
+                    console.log("Init stores");
+                    //TODO: WOW, this is ugly AF
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] = createStore(AppReducer, {
+                            Tags,
+                            BlogPost: curr,
+                            LoggedIn: false
+                        });
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initStore();
+            this.storesLogged = {
+                initStoresLogged: function () {
+                    console.log("Init storesLogged")
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] = createStore(AppReducer, {
+                            Tags,
+                            BlogPost: curr,
+                            LoggedIn: true
+                        });
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initStoresLogged();
+            this.contents = {
+                initContent: function (stores) {
+                    console.log("Init contents")
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] = ReactDOM.renderToString(
+                            <Provider
+                                store={stores[encodeURIComponent(curr.title)]}><ReactApp><BlogPost/></ReactApp></Provider>
+                        );
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initContent(this.stores);
+            this.contentsLogged = {
+                initContentsLogged: function (storesLogged) {
+                    console.log("Init contentsLogged")
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] = ReactDOM.renderToString(
+                            <Provider
+                                store={storesLogged[encodeURIComponent(curr.title)]}><ReactApp><BlogPost/></ReactApp></Provider>
+                        );
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initContentsLogged(this.storesLogged);
+            this.responses = {
+                initResponse: function (stores, contents) {
+                    console.log("Init responeses")
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] =
+                            renderHTML(contents[encodeURIComponent(curr.title)], stores[encodeURIComponent(curr.title)].getState());
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initResponse(this.stores, this.contents);
+            this.responsesLogged = {
+                initResponseLogged: function (storesLogged, contentsLogged) {
+                    console.log("Init responsesLogged")
+                    return _.reduce((acc, curr)=> {
+                        acc[encodeURIComponent(curr.title)] =
+                            renderHTML(contentsLogged[encodeURIComponent(curr.title)], storesLogged[encodeURIComponent(curr.title)].getState());
+                        return acc
+                    }, this, queryCache.Items)
+                }
+            }.initResponseLogged(this.storesLogged, this.contentsLogged);
+            this.getResponseWithBlogPost = function (loggedIn, blogPostURI) {
+                //TODO: Maybe all the blogPosts should be a propety of the main objects, instead of changing the getResponse function pattern?
+                return loggedIn ? this.responsesLogged[blogPostURI] : this.responses[blogPostURI];
+            };
+            return this;
+        }
+    },//TODO: Cache as good as possible. response, store and content should be objects with blogpost name props, in which you could filter easily
+};
+fillQueryCache().then(getTags().then((tags)=> {
+    Object.keys(pathsAndStores).forEach((key, index)=> {
+        if (pathsAndStores[key].hasOwnProperty('init')) {
+            console.log("initializing " + key);
+            pathsAndStores[key].init();
+        }
+    });
+}))
 
 //*******************************************************END OF DB SETUP************************************************
-export {queryBlogPosts, blogPostToDb, Tags, getTags}
+export {queryBlogPosts, blogPostToDb, Tags, getTags, pathsAndStores}
--- src/server/routes/gets.js
+++ src/server/routes/gets.js
@@ -22,55 +22,8 @@ import Blog from "../../public/pages/Blog"
 import BlogPost from "../../public/pages/BlogPost"
 import AppReducer from "../../public/reducers/StoreAndReducers"
 
-import {Tags, getTags} from '../db'
+import {pathsAndStores} from '../db'
 import {createStore} from 'redux';
-let pathsAndStores = {
-    '/': {
-        init: function () {
-            this.store = createStore(AppReducer, {Tags, LoggedIn: false});
-            this.storeLogged = createStore(AppReducer, {Tags, LoggedIn: true});
-            this.content = ReactDOM.renderToString(
-                <Provider store={this.store}><ReactApp><About/></ReactApp></Provider>);
-            this.contentLogged = ReactDOM.renderToString(
-                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
-            this.response = renderHTML(this.content, this.store.getState());
-            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
-            this.getResponse = function (loggedIn) {
-                return loggedIn ? this.responseLogged : this.response;
-            };
-            return this;
-        },
-    },
-    '/admin:': {},
-    '/admin/:blogTitle': {},
-    '/about': {},
-    '/contact': {},
-    '/blog': {
-        init: function () {
-            this.store = createStore(AppReducer, {Tags,});
-            this.storeLogged = createStore(AppReducer, {Tags});
-            this.content = ReactDOM.renderToString(
-                <Provider store={this.store}><ReactApp><Blog/></ReactApp></Provider>);
-            this.contentLogged = ReactDOM.renderToString(
-                <Provider store={this.storeLogged}><ReactApp><Blog/></ReactApp></Provider>);
-            this.response = renderHTML(this.content, this.store.getState());
-            this.responseLogged = renderHTML(this.contentLogged, this.storeLogged.getState());
-            this.getResponse = function (loggedIn) {
-                return loggedIn ? this.responseLogged : this.response;
-            };
-            return this;
-        },
-    },
-    '/blog/:blogTitle': {},
-    '/projects': {}
-};
-getTags().then((tags)=> {
-    Object.keys(pathsAndStores).forEach((key, index)=> {
-        if (pathsAndStores[key].hasOwnProperty('init')) {
-            pathsAndStores[key].init();
-        }
-    });
-});
 
 
 let router = express.Router();
@@ -90,17 +43,9 @@ router.get('/admin', (req, res) => {//TODO:HTTPS
         path: req.path
     });
     let content = "";
-    checkHash(req.cookies.name, req.cookies.hash).then((result)=> {
-        if (result)
-            content = ReactDOM.renderToString(<Provider
-                store={store}><ReactApp><AdminLoggedIn></AdminLoggedIn></ReactApp></Provider>);
-        else
-            content = ReactDOM.renderToString(<Provider store={store}><ReactApp><Admin/></ReactApp></Provider>);
-        return;
-    }).then(()=> {
-        let response = renderHTML(content, initialState);
-        res.send(response);
-    })
+    checkHash(req.cookies.name, req.cookies.hash).then(result=>
+        res.send(pathsAndStores['/admin'].getResponse(result))
+    )
 });
 router.get('/admin/:blogTitle', (req, res) => {//TODO:Better regex, only match /string_like_this
     "use strict";
@@ -108,46 +53,22 @@ router.get('/admin/:blogTitle', (req, res) => {//TODO:Better regex, only match /
         reuqestType: "GET",
         path: req.path
     });
-    checkHash(req.cookies.name, req.cookies.hash).then((result)=> {
-        if (result) {
-            //store.dispatch(loadBlogPost(getBlogPostByTitle(decodeURIComponent(req.params.blogTitle)))); //TODO:better way
-            let content = ReactDOM.renderToString(<Provider
-                store={store}><ReactApp><AdminLoggedIn></AdminLoggedIn></ReactApp></Provider>);
-            let response = renderHTML(content, initialState);
-            res.send(response);
-        }
-    })
-});
-router.get('/about', (req, res) => {
-    "use strict";
-    console.log({
-        reuqestType: "GET",
-        path: req.path
-    });
-    let content = ReactDOM.renderToString(<Provider store={store}><ReactApp/></Provider>);
-    let response = renderHTML(content, initialState);
-    res.send(response);
+
+    checkHash(req.cookies.name, req.cookies.hash).then(result=>
+        res.send(pathsAndStores['/admin/:blogTitle'].getResponse(result))
+    )
 });
 
-router.get('/contact', (req, res) => {
-    "use strict";
-    console.log({
-        reuqestType: "GET",
-        path: req.path
-    });
-    let appContent = ReactDOM.renderToString(React.createElement(ReactApp));
-    res.render('cv', {content: appContent});
-});
 router.get('/blog/:blogTitle', (req, res) => {//TODO:Better regex, only match /string_like_this
     "use strict";
     console.log({
         reuqestType: "GET",
         path: req.path
     });
-    store.dispatch(loadBlogPost(getBlogPostByTitle(decodeURIComponent(req.params.blogTitle))));
-    let content = ReactDOM.renderToString(<Provider store={store}><ReactApp><BlogPost/></ReactApp></Provider>);
-    let response = renderHTML(content, store.getState());
-    res.send(response);
+
+    checkHash(req.cookies.name, req.cookies.hash).then(result=>
+        res.send(pathsAndStores['/blog/:blogTitle']
+            .getResponseWithBlogPost(result, encodeURIComponent(req.params.blogTitle))));
 });
 
 router.get('/blog', (req, res) => {//TODO:Better regex, only match /string_like_this
@@ -162,15 +83,6 @@ router.get('/blog', (req, res) => {//TODO:Better regex, only match /string_like_
 
 });
 
-router.get('/projects', (req, res) => {
-    "use strict";
-    console.log({
-        reuqestType: "GET",
-        path: req.path
-    });
-    let appContent = ReactDOM.renderToString(React.createElement(ReactApp));
-    res.render('cv', {content: appContent});
-});
 /* This is the handler for hiding admin side scripts from client*/
 router.get('/private/script.js', (req, res) => {
 
